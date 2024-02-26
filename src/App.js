fdbda76e27a545a031caab7f611ba3fe87cc5073
Shivagiii <shivangiadeshara@gmail.com>
2024-02-26 15:29:09 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import {fetchCity, fetchCountry,fetchState} from './apiFetch';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [list,setList] = useState({
+    countryList:[],
+    stateList:[],
+    cityList:[]
+
+
+  })
+  const [selected,setSelected] = useState({
+    selectedCountry:'',
+    selectedState:'',
+    selectedCity:''
+
+  })
+
+  useEffect(() =>{
+    if(fetchCountry){
+      fetchCountry().then((res) => {
+        setList({...list,countryList:[...res]})
+      })
+    }
+   },[])
+
+ useEffect(() =>{
+  if(selected.selectedCountry){
+
+    fetchState(selected.selectedCountry).then((res) => {
+      //console.log(res);
+      setList({...list,stateList:[...res]})
+    })  
+  }
+
+ },[selected.selectedCountry])
+
+ useEffect(() =>{
+  if(selected.selectedCountry && selected.selectedState){
+
+    fetchCity(selected.selectedCountry,selected.selectedState).then((res) => {
+      //console.log(res);
+      setList({...list,cityList:[...res]})
+    })  
+  }
+
+ },[selected.selectedState])
+
+function sCountry(e){
+  //console.log(e.target.name)
+  setSelected( {...selected,
+    [e.target.name]:document.getElementById(e.target.id).value});
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Select Location</h2>
+      <select id='country' onChange={sCountry} name='selectedCountry'>
+        <option selected>Select Country</option>
+        {list.countryList.map((country) => (
+          <option value={country}>{country}</option>
+        ))
+        }
+      </select>
+      <select id='state' disabled={selected.selectedCountry ? '':'disabled'} name='selectedState' onChange={sCountry}>
+        <option selected>Select State</option>
+        {list.stateList.map((state) => (
+          <option value={state}>{state}</option>
+        ))
+        }
+      </select>
+      <select id='city' disabled={selected.selectedCountry && selected.selectedState ? '':'disabled' } name='selectedCity' onChange={sCountry}>
+        <option selected>Select City</option>
+        {list.cityList.map((city) => (
+          <option value={city}>{city}</option>
+        ))
+        }
+      </select>
+<br/>
+{selected.selectedCity ? 
+  <div>
+      <h4>You selected </h4><h2>{selected.selectedCountry}, </h2><h4>{selected.selectedState}, {selected.selectedCity}</h4>
+      </div>
+      :
+      <></>}
     </div>
   );
 }
---
+++ src/apiFetch.js
@@ -0,0 +1,36 @@
+import axios from "axios"
+
+const ENDPOINT="https://crio-location-selector.onrender.com"
+
+export const fetchCountry = async() => {
+    try{
+        const {data}= await axios.get(`${ENDPOINT}/countries`);
+        //console.log(data)
+        return data
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+export const fetchState = async(country) => {
+    console.log(country)
+    try{
+        const {data}= await axios.get(`${ENDPOINT}/country=${country}/states`);
+        
+        return data
+    }
+    catch(e){
+        console.log(e)
+    }
+}
+export const fetchCity = async(country,state) => {
+    //console.log(country)
+    try{
+        const {data}= await axios.get(`${ENDPOINT}/country=${country}/state=${state}/cities`);
+        
+        return data
+    }
+    catch(e){
+        console.log(e)
+    }
+}
